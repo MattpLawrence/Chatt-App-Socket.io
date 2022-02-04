a26617e794caf5d7b71c6dceaf11115f680f4f3e
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-04 12:30:09 -0500

    main.js chat message event handled, moving to server.js to catch

--- public/js/main.js
+++ public/js/main.js
@@ -9,4 +9,10 @@ socket.on("message", (message) => {
 // massage submit
 chatForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  // grab the value of the message when submitted
+  const msg = e.target.elements.msg.value;
+
+  // emit the submitted massage to the server
+  socket.emit("chatMessage", msg);
 });
