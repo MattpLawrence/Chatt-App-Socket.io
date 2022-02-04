015198a2cf2f2f793b11369599488cf4d3308a2b
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-04 12:19:26 -0500

    adding socket.emit

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.use(express.static(path.join(__dirname, "public")));
 // run when client connects
 io.on("connection", (socket) => {
   console.log("new connection");
+
+  //emit object to be grabbed with socket.on() in main.js
+  socket.emit("message", "Welcome to Gamer Gabble");
 });
 
 const PORT = process.env.PORT || 3000;
