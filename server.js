b168566b323c3612c18d7cff2b332d66a98c7482
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-04 12:16:56 -0500

    update server.js and chat.html script to connect

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const path = require("path");
+//needed to run socket.io
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = process.env.PORT || 300;
+// run when client connects
+io.on("connection", (socket) => {
+  console.log("new connection");
+});
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
