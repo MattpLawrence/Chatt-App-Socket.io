997e60a0a59a1ce687f1d7b3209555380a8cb857
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-08 16:41:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -88,4 +88,10 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+// server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(` \nServer listening on port: ${PORT}`);
+  });
+});
